218be577dfde9f188772cca1f845f36ce7ca43cb
Walter Fan <walter.fan@zoom.us>
2025-01-12 17:00:57 +0800

    update readme and script

--- README.md
+++ README.md
@@ -1,9 +1,52 @@
-# video_to_text
+# video text
 
-extract audio from video file, then do Automatic Speech Recognition, and translate the text
 
-## Usage
+A python script to convert mp4 video to text using whisper,
+and translate the text from source language("en" by default) to destination language("zh-cn" by default).
+
+
+## usage
 
 ```bash
-./video_to_text.py test.mp4 asr_output.txt
+./video_to_text.py -h
+usage: video_to_text.py [-h] --input INPUT [--outout OUTPUT] [--model MODEL] [--src SRC] [--dest DEST] [--format FORMAT]
+
+Convert MP4 video to text using Whisper, and translate the text.
+
+options:
+  -h, --help            show this help message and exit
+  --input INPUT, -i INPUT
+                        path to the input mp4 file
+  --outout OUTPUT, -o OUTPUT
+                        path to the output text file, it is the same as the input file with extenstion ".srt" if not specified
+  --model MODEL, -m MODEL
+                        the model that whisper will use, it is "small" by default
+  --src SRC, -s SRC     source language, it is "en" by default
+  --dest DEST, -d DEST  target language, it is "zh-cn" by default
+  --format FORMAT, -f FORMAT
+                        output format, only support "txt" or "srt" format for now
+
+```
+
+for example
+
+```bash
+./video_to_text.py -i ./example/5_minutes_for_50_years.mp4
+```
+
+
+## tips
+
+You can download any video from youtube or other site, and then use the script to make subtitles.
+
+1. use https://cobalt.tools/ to download the video you want
+
+2. trim the video
+```
+ffmpeg -i input.mp4 -ss 00:00:07 -c:v copy -c:a copy output_trimmed.mp4
+
+```
+3. convert the video resolution
+```
+ffmpeg -i output_trimmed.mp4 -vf scale=320:180 -c:v libx264 -preset fast -crf 23 -c:a aac -b:a 192k output_180p.mp4
 ```
--- video_to_text.py
+++ video_to_text.py
@@ -7,6 +7,19 @@ import argparse
 import asyncio
 from googletrans import Translator
 
+def format_time(seconds):
+    millis = int((float(seconds) % 1) * 1000)
+    seconds = int(float(seconds))
+    mins, secs = divmod(seconds, 60)
+    hrs, mins = divmod(mins, 60)
+    return f"{hrs:02}:{mins:02}:{secs:02},{millis:03}"
+
+
+def extract_file_info(file_path):
+    folder_path, file_name_with_ext = os.path.split(file_path)
+    file_name, file_extension = os.path.splitext(file_name_with_ext)
+    return folder_path, file_name, file_extension
+
 def extract_audio_from_video(mp4_file, audio_file):
     """Extract audio from an MP4 file and save as WAV."""
     ffmpeg.input(mp4_file).output(audio_file).run()
@@ -21,32 +34,41 @@ def transcribe_audio_with_whisper(audio_file, model_name="base"):
     result = model.transcribe(audio_file)
     return result["text"]
 
-def transcribe_audio_with_segments(audio_file, model_name="base", pause_threshold=1.0):
+def transcribe_audio_with_segments(audio_file, model_name="base", pause_threshold=0.5):
 
     model = whisper.load_model(model_name)
-    
+
     result = model.transcribe(audio_file, word_timestamps=True)
     
     segments = result["segments"]
-    
+    sn = 0
     paragraphs = []
     current_paragraph = []
     previous_end_time = 0.0
-    
+    paragraph_start_time = 0.0
+
     for segment in segments:
         start_time = segment["start"]
         end_time = segment["end"]
         text = segment["text"].strip()
-        
+
+        # the first paragraph
+        if len(current_paragraph) == 0:
+            paragraph_start_time = start_time
+
         # create new paragraph if time distance is greater than pause threshold
         if start_time - previous_end_time > pause_threshold:
             if current_paragraph:
-                paragraphs.append(" ".join(current_paragraph))
+                sn += 1
+                merged_text = " ".join(current_paragraph)
+                paragraphs.append(f"\n{sn}\n[{format_time(paragraph_start_time)} --> {format_time(end_time)}]\n{merged_text}")
+                # start new paragraph
                 current_paragraph = []
+                paragraph_start_time = start_time
         
         current_paragraph.append(text)
         previous_end_time = end_time
-    
+
     # the last paragraph
     if current_paragraph:
         paragraphs.append(" ".join(current_paragraph))
@@ -54,47 +76,57 @@ def transcribe_audio_with_segments(audio_file, model_name="base", pause_threshol
     return paragraphs
 
 
-async def translate_text(text, src="en", dest="zh-cn"):
+async def translate_text(text, src, dest):
     """Translate text from English to Chinese."""
     translator = Translator()
     translated = await translator.translate(text, src=src, dest=dest)
     return translated.text
 
-def do_translate(text_file, text):
-    translated_text = asyncio.run(translate_text(text))
-    with open(text_file.rsplit(".", 1)[0] + "_cn.txt", "w", encoding="utf-8") as f:
+def do_translate(text_file, text, src, dest):
+    translated_text = asyncio.run(translate_text(text, src, dest))
+    folder_path, file_name, file_extension = extract_file_info(text_file)
+    with open(os.path.join(folder_path, file_name + "_" + dest + file_extension), "w", encoding="utf-8") as f:
         f.write(translated_text)
     print("-"*80)
     print(f"Transcribed Text:\n {translated_text}")
 
-def do_asr(text_file, audio_file):
+def do_asr(audio_file: str, text_file: str, model_name: str):
     print("Transcribing audio with Whisper...")
-    text = transcribe_audio_with_whisper(audio_file, model_name="base")  # Use "base", "small", or larger models as needed
-    paragraphs = transcribe_audio_with_segments(audio_file, model_name="base", pause_threshold=1.5)
+    #text = transcribe_audio_with_whisper(audio_file, model_name="base")  # Use "base", "small", or larger models as needed
+    paragraphs = transcribe_audio_with_segments(audio_file, model_name=model_name, pause_threshold=1.5)
     text = "\n".join(paragraphs)
     with open(text_file, "w", encoding="utf-8") as f:
         f.write(text)
     print(f"ASR Text:\n{text}")
     return text
 
-def main(mp4_file, text_file):
-    audio_file = mp4_file.rsplit(".", 1)[0] + ".wav"
+def main(video_file: str, text_file: str, model_name: str , src: str, dest: str):
+    file_path = video_file.rsplit(".", 1)[0]
+    audio_file =  file_path + "_temp.wav"
+    if not text_file:
+        text_file = f"{file_path}_{src}.srt"
 
-    print(f"Extracting audio {audio_file} from {mp4_file}...")
-    extract_audio_from_video(mp4_file, audio_file)
-
-    text = do_asr(text_file, audio_file)
-
-    do_translate(text_file, text)
+    print(f"1. Extracting audio {audio_file} from {video_file}...")
+    extract_audio_from_video(video_file, audio_file)
+    print(f"2. Recognizing text from audio {audio_file} to {text_file}...")
+    text = do_asr(audio_file, text_file, model_name)
+    print(f"3. Translate text file {text_file} from {src} to {dest}")
+    do_translate(text_file, text, src, dest)
 
     # Clean up temporary files
     if os.path.exists(audio_file):
         os.remove(audio_file)
 
 if __name__ == "__main__":
+
     parser = argparse.ArgumentParser(description="Convert MP4 video to text using Whisper, and translate the text.")
-    parser.add_argument("input", type=str, help="Path to the input mp4 file")
-    parser.add_argument("output", type=str, help="Path to the output text file")
+    parser.add_argument('--input','-i', required=True,  dest='input', help='path to the input mp4 file')
+    parser.add_argument('--outout', '-o', dest='output', help='path to the output text file, it is the same as the input file with extenstion ".srt" if not specified')
+    parser.add_argument('--model', '-m', dest='model', default="small", help='the model that whisper will use, it is "small" by default')
+    parser.add_argument('--src', '-s', dest='src', default="en", help='source language, it is "en" by default')
+    parser.add_argument('--dest', '-d', dest='dest', default="zh-cn", help='target language, it is "zh-cn" by default')
+    parser.add_argument('--format', '-f', dest='format', default="srt", help='output format, only support "txt" or "srt" format for now')
+
     args = parser.parse_args()
 
-    main(args.input, args.output)
+    main(args.input, args.output, args.model, args.src, args.dest)
